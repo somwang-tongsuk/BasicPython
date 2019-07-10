d8744e24fe672f61db6e7217f1592ad53e1a9abd
somwang <somwangthongsuk@gmail.com>
2019-07-10 12:13:48 +0700

    edit not show img

--- store/settings.py
+++ store/settings.py
@@ -121,7 +121,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+django_heroku.settings(locals())
