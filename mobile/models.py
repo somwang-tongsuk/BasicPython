8449cbec280ec1d8191840ae3ed4b5fd46bf7cea
somwang <somwangthongsuk@gmail.com>
2019-07-10 15:28:02 +0700

    add Model Product, EMS

--- mobile/models.py
+++ mobile/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	product_name = models.CharField(max_length=120)
+	product_desc = models.TextField(null=True)
+
+	def __str__(self):
+		return self.product_name #ดึงตัวแปลข้างนอกมาใช้งานได้
+
+class EMS(models.Model):
+	ems_number = models.CharField(max_length=120)
+	ems_name = models.CharField(max_length=120)
+
+	def __str__(self):
+		return 'Name: {} EMS: {}'.format(self.ems_name,self.ems_number)
--- mobile/views.py
+++ mobile/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Product, EMS
 # Create your views here.
 
 def Home(request):
 	#return HttpResponse('<h1>Hello World</h1>')
-	return render(request, 'mobile/home.html')
+	allmobile = Product.objects.all()
+	#context = {'phonelist':['I Phone XI','Huawei','Samsung','Oppo']}
+	context = {'phonelist':allmobile}
+	return render(request, 'mobile/home.html', context)
 
 def About(request):
 	#return HttpResponse('<h1>About Us</h1>')
