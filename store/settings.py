25d9fd6ba9dbd929dd819889f851d2201b456784
somwangthongsuk <somwangthongsuk@gmail.com>
2019-08-03 14:33:33 +0700

    heroku django debug false 500

--- store/settings.py
+++ store/settings.py
@@ -27,11 +27,26 @@ SECRET_KEY = '#!$^--+rsa2c!l0p=x(mbslzcx@tr*#r1+-i$ub$1ju+bd(lf8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['python-basic-demo.herokuapp.com']
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
+ALLOWED_HOSTS = ['.herokuapp.com, 127.0.0.1']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'crispy_forms',
     'mobile.apps.MobileConfig',
